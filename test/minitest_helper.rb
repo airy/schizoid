ec7552ec809ca685cc366af76022235424537ee7
airy <airydragon@gmail.com>
2013-03-14 04:45:39 +0200

    dummy actor class added to minitest_helper.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -12,3 +12,10 @@ $:<<'app/models'    # add models path to load path
 $:<<'app/roles'     # add roles path to load path
 $:<<'app/contexts'  # add contexts load path
 
+
+# Dummy class Actor (accepts roles)
+require 'schizo'
+
+class Actor
+  include Schizo::Data
+end
